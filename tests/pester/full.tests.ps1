4e9b8c668b8c37ecf0eec6eae37e61581ed30d9d
Jack Tracey <jack@jacktracey.co.uk>
2023-10-11 16:33:38 +0100

    try again

--- tests/pester/full.tests.ps1
+++ tests/pester/full.tests.ps1
@@ -65,10 +65,9 @@ Describe "Bicep Landing Zone (Sub) Vending Tests" {
     It "Should Have a Role Assignment for an known AAD Group with the Reader role directly upon the Subscription" {
       $iterationCount = 0
       do {
-        $allRoleAssignmentsSub = Get-AzRoleAssignment -Scope "/subscriptions/$subId" -ErrorAction SilentlyContinue
-        $roleAssignment = $allRoleAssignmentsSub | Where-Object { $_.ObjectId -eq "7eca0dca-6701-46f1-b7b6-8b424dab50b3" -and $_.RoleDefinitionName -eq "Reader" }
+        $roleAssignment = Get-AzRoleAssignment -Scope "/subscriptions/$subId" -ObjectId "7eca0dca-6701-46f1-b7b6-8b424dab50b5" -RoleDefinitionName "Reader" -ErrorAction SilentlyContinue
         if ($null -eq $roleAssignment) {
-          Write-Host "Waiting for Role Assignments to be eventually consistent... Iteration: $($iterationCount)" -ForegroundColor Yellow
+          Write-Host "Waiting for Subscription Role Assignments to be eventually consistent... Iteration: $($iterationCount)" -ForegroundColor Yellow
           Start-Sleep -Seconds 15
           $iterationCount++
         }
@@ -84,10 +83,9 @@ Describe "Bicep Landing Zone (Sub) Vending Tests" {
     It "Should Have a Role Assignment for an known AAD Group with the Network Contributor role directly upon the Resource Group" {
       $iterationCount = 0
       do {
-        $allRoleAssignmentsRsg = Get-AzRoleAssignment -Scope "/subscriptions/$subId/resourceGroups/rsg-$location-net-hs-pr-$prNumber" -ErrorAction SilentlyContinue
-        $roleAssignment = $allRoleAssignmentsRsg | Where-Object { $_.ObjectId -eq "7eca0dca-6701-46f1-b7b6-8b424dab50b3" -and $_.RoleDefinitionName -eq "Network Contributor" }
+        $roleAssignment = Get-AzRoleAssignment -Scope "/subscriptions/$subId/resourceGroups/rsg-$location-net-hs-pr-$prNumber" -ObjectId "7eca0dca-6701-46f1-b7b6-8b424dab50b5" -RoleDefinitionName "Network Contributor" -ErrorAction SilentlyContinue
         if ($null -eq $roleAssignment) {
-          Write-Host "Waiting for Role Assignments to be eventually consistent... Iteration: $($iterationCount)" -ForegroundColor Yellow
+          Write-Host "Waiting for Resource Group Role Assignments to be eventually consistent... Iteration: $($iterationCount)" -ForegroundColor Yellow
           Start-Sleep -Seconds 15
           $iterationCount++
         }
